6ee4aa2e64fdb44a707e4783452931126e22d5e5
Sagar Verma <105728547+Optimus5734@users.noreply.github.com>
2023-08-21 00:09:25 +0530

    Update (153) Minimum _in_Rotated_Sorted_Array.java

--- (153) Minimum _in_Rotated_Sorted_Array.java
+++ (153) Minimum _in_Rotated_Sorted_Array.java
@@ -1,24 +1,30 @@
-153. Find Minimum in Rotated Sorted Array
-
-
-
-
+//153. Find Minimum in Rotated Sorted Array
+/*
 # Intuition
-<!-- Describe your first thoughts on how to solve this problem. -->
-the point that is minimum must have 2 criteria 1 it must be smaller from both inedx+1 and index-1 Thats what we are searching find a mid and see if it is smaller than mid-1 that means we are in position where rotatio have occur
-if mid+1 is smaller than mid then mid +1 is the space for rotation
+Here we are iterating through all possible substrings of the string, 
+counting the frequency of characters in each substring, and then
+finding the difference between the maximum and minimum frequencies 
+of characters in that substring.
 # Approach
-<!-- Describe your approach to solving the problem. -->
-if not that remove the 1 side of the sorted array
+# 1. Beauty Function (beauty):
+-->takes an array freq representing the frequency of characters in a substring.
+-->It iterates through the freq array to find the minimum and maximum frequency of characters.
+-->The difference between the maximum and minimum frequencies is returned and yes ignore it count is 0;
+# 2. Beauty Sum Function (beautySum):
+--> initializes ans to store the beauty sum.
+-->It uses two nested loops to iterate through all possible substrings of the string:
+-->The outer loop iterates over the starting index and inner loop   iterates over the ending index of the substring.
+-->Inside the inner loop, a frequency array frequency is maintained to track the frequency of characters in the current substring.
+-->beauty function :-- calculate the beauty value for the current substring.and then added to the ans variable.
+-->The final ans value represents the beauty sum of all possible substrings.
 # Complexity
 - Time complexity:
 <!-- Add your time complexity here, e.g. $$O(n)$$ -->
-O(lgn)
+$$O(n*n)$$
 - Space complexity:
 <!-- Add your space complexity here, e.g. $$O(n)$$ -->
-O(1)
-# Code
-```
+$$O(n)$$
+*/
 class Solution {
     public int findMin(int[] nums) {
         int l=0,h=nums.length-1,mid,ans=Integer.MAX_VALUE;
